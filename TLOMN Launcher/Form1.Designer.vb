3ba283d3f0a2fe4a61d51c3932d61371ecebb3c7
Ben Garcia <pokeboygarcia@gmail.com>
2018-02-21 15:54:37 -0800

    Initial code upload.

---
+++ TLOMN Launcher/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 494)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(655, 55)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Play: BIONICLE: The Legend of Mata Nui"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 12)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(860, 476)
+        Me.WebBrowser1.TabIndex = 2
+        Me.WebBrowser1.Url = New System.Uri("http://biomediaproject.com/bmp/files/gms/tlomn/Launcher/Web/PatchNotes.html", System.UriKind.Absolute)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(686, 494)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(186, 55)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Activate script for easier debug option control"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 561)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "BIONICLE: The Legend of Mata Nui Launcher"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button2 As Button
+End Class
---
+++ TLOMN Launcher/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If My.Computer.FileSystem.FileExists("LEGO Bionicle.exe") Then
+            My.Computer.Network.DownloadFile(
+            "http://biomediaproject.com/bmp/files/gms/tlomn/Launcher/Patch/version.txt",
+            "Temp\version.txt", "", "", True, 1000, True)
+            Dim text1 As String = My.Computer.FileSystem.ReadAllText("Temp\version.txt")
+            Dim text2 As String = My.Computer.FileSystem.ReadAllText("version.txt")
+            If text1 <> text2 Then
+                MessageBox.Show("put your text here", "here the title of you message",
+                                MessageBoxButtons.OKCancel, MessageBoxIcon.Hand)
+                My.Computer.Network.DownloadFile(
+               "http://biomediaproject.com/bmp/files/gms/tlomn/Launcher/Patch/Beaverhouse%20TLOMN%20Patch.exe",
+               "Temp\Beaverhouse TLOMN Patch.exe", "", "", True, 1000, True)
+                Process.Start("Temp\Beaverhouse TLOMN Patch.exe").WaitForExit()
+            End If
+            My.Computer.FileSystem.DeleteDirectory("Temp", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            Process.Start("LEGO Bionicle.exe")
+        Else
+            My.Computer.Network.DownloadFile(
+            "http://biomediaproject.com/bmp/files/gms/tlomn/Launcher/setup.exe",
+            "Temp\setup.exe", "", "", True, 1000, True)
+            Process.Start("Temp\TLOMN\setup.exe").WaitForExit()
+            My.Computer.Network.DownloadFile(
+                       "http://biomediaproject.com/bmp/files/gms/tlomn/Launcher/Patch/Beaverhouse%20TLOMN%20Patch.exe",
+                       "Temp\Beaverhouse TLOMN Patch.exe", "", "", True, 1000, True)
+            Process.Start("Temp\Beaverhouse TLOMN Patch.exe").WaitForExit()
+            My.Computer.FileSystem.DeleteDirectory("Temp", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            Process.Start("LEGO Bionicle.exe")
+        End If
+        MyBase.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("Debug Menu Fix.exe")
+    End Sub
+End Class
